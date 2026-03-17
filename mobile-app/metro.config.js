d906d3767cd4d64fa629ee42e43ddac7d5775aaa
lkaydhillon <lkaydhillon@gmail.com>
2026-03-17 17:02:32 +0530

    chore: simplify frontend folder name to app for Vercel

--- mobile-app/metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
-
-const config = getDefaultConfig(__dirname);
-
-// Add WASM support for expo-sqlite on Web
-config.resolver.assetExts.push('wasm');
-
-module.exports = withNativeWind(config, { input: "./global.css" });
